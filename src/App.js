edccc0c811d29af73853ae6c678b4af9e24eb81d
kioria95 <kenkioria@gmail.com>
2020-10-16 17:06:16 +0300

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="app">
-      {/*Header */}
       <Header />
+      <Router>
+        {/*Header */}
 
-      {/*Tinder Cards */}
+        <Switch>
+          <Route path="/chat">
+            <h1>I am chat</h1>
+          </Route>
+          <Route path="/">
+            <TinderCards />
+          </Route>
+        </Switch>
 
-      {/*Tinder Buttons */}
-      
+        {/*Tinder Cards */}
+
+        {/*Tinder Buttons */}
+      </Router>
     </div>
   );
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TinderCards.css'
+
+
+
+function TinderCards() {
+    return (
+        <div className="tinderCards">
+            
+        </div>
+    )
+}
+
+export default TinderCards
