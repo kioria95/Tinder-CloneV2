c0239418e0c4090a4132edc3564139b1b58ea16c
kioria95 <kenkioria@gmail.com>
2020-10-16 16:56:06 +0300

    Finished on Header.js and Header.css

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import './Header.css'
+import { IconButton } from "@material-ui/core";
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
-    return (
-        <div className="header">
-            <h1>Header</h1>
-        </div>
-    )
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" />
+      </IconButton>
+
+      <img
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt="tinderlogo"
+      />
+
+      <IconButton>
+        <ForumIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
